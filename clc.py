6840bdeeb058e4ac76cd0a3eda3c9b66da2bf762
science hacks <sciencefictionut@gmail.com>
2024-12-28 05:49:46 +0000

    list entering

---
+++ clc.py
@@ -0,0 +1,15 @@
+def right():
+ l_1=[]
+ for i in range(1000):
+    print(f'input num {i}') 
+    a=input("pls enter something")
+    if (a=='Y') or (a=='y'):
+        break
+    l_1.append(a)
+ print(l_1)
+ print(type(l_1        ))
+right()
+
+
+    
+
